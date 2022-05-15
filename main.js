3d66c7184311849dc1deb167f0c30fc86822eaba
ali <alioliaee31@gmail.com>
2022-05-15 15:27:13 +0430

    calculate hash

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+import SHA256 from "crypto-js/sha256";
 class Block {
   constructor(index, timestamp, data, previousHash = "") {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
   }
 
   calculateHash() {
-    return;
+    return SHA256(
+      this.index,
+      this.timestamp,
+      JSON.stringify(this.data)
+    ).toString();
   }
 }
