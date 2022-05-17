f89a0718bb5994fa534b4621a31ebb2272b9aa3e
ali <alioliaee31@gmail.com>
2022-05-17 11:07:02 +0430

    create react app

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
--- src/keygenerator.js
+++
@@ -1,9 +0,0 @@
-const EC = require("elliptic").ec;
-const ec = new EC("secp256k1");
-
-const key = ec.genKeyPair();
-const publicKey = key.getPublic("hex");
-const privateKey = key.getPrivate("hex");
-
-console.log("publicKey", publicKey);
-console.log("privateKey", privateKey);
