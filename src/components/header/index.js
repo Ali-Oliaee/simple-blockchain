db88cf12ed57aa43ea15cb2a303f4e0c7a4278ee
ali <alioliaee31@gmail.com>
2022-05-17 11:49:45 +0430

    add header

---
+++ src/components/header/index.js
@@ -0,0 +1,27 @@
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="md">
+      <Container fluid>
+        <Navbar.Brand href="/">Ali's ShitCoin blockchain</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+          <Nav navbarScroll>
+            <Nav.Link href="/">
+              <Button variant="outline-light">Settings</Button>
+            </Nav.Link>
+            <Nav.Link href="/">
+              <Button variant="outline-light">Create transaction</Button>
+            </Nav.Link>
+            <Nav.Link href="/">
+              <Button variant="outline-light">Pending transactions</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
