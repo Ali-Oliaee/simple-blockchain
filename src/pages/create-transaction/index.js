894101796c613d55efb05c4ce4d2e32c70fa8869
ali <alioliaee31@gmail.com>
2022-05-17 15:19:31 +0430

    create pending transaction page

--- src/pages/create-transaction/index.js
+++ src/pages/create-transaction/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Container, Form, InputGroup } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 
 function CreateTransactionPage() {
   const [validated, setValidated] = useState(false);
--- src/pages/pending-transactions/index.js
+++ src/pages/pending-transactions/index.js
@@ -1,5 +1,18 @@
+import { Button } from "react-bootstrap";
+import TransactionTable from "../../components/transactions-table";
+
 function PendingTransactionsPage() {
-  return <div>PendingTransactionsPage</div>;
+  return (
+    <div className="pending-transaction-page p-4">
+      <h1>Pending Transactions</h1>
+      <p>
+        This transactions are waiting to be included in the next block. Next
+        block is created when you start the mining process.
+      </p>
+      <TransactionTable />
+      <Button>Start mining</Button>
+    </div>
+  );
 }
 
 export default PendingTransactionsPage;
