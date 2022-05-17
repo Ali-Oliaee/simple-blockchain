7b70650bbf83f0b1e7514e3af645ed9aac681e98
ali <alioliaee31@gmail.com>
2022-05-17 20:04:03 +0430

    add wallet details page

--- src/pages/wallet/index.js
+++ src/pages/wallet/index.js
@@ -1,5 +1,20 @@
+import TransactionTable from "../../components/transactions-table";
+
 function WalletPage() {
-  return <div>WalletPage</div>;
+  const url = window.location.href.split("/");
+  const walletAddress = url[url.length - 1];
+
+  return (
+    <div className="wallet-page p-4">
+      <h1>Wallet details</h1>
+      <h3>Address: </h3>
+      <span>{walletAddress}</span>
+      <h3>Balance: </h3>
+      <span>{1200}</span>
+      <hr />
+      <TransactionTable />
+    </div>
+  );
 }
 
 export default WalletPage;
