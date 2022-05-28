bd88008068ccedc3558128db707878607f25183f
ali <alioliaee31@gmail.com>
2022-05-28 22:01:16 +0430

    some cleanups

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
